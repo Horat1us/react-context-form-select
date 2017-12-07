fe432aeeba6db051073c77ab6ecc7d3dd212ca24
Alexander Letnikow <reclamme@gmail.com>
2017-12-07 18:42:37 +0200

    Implement AsyncSelect

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -3,8 +3,9 @@ import * as PropTypes from "prop-types";
 import { default as PlainSelect, ReactSelectProps, OptionValues, Option, OnChangeHandler, } from "react-select";
 
 import { InputContextTypes, InputContext } from "react-context-form";
+import { handleChange } from "../helpers/handleChange";
 
-export const SelectDefaultProps = {
+export const SelectDefaultProps: {[K in keyof ReactSelectProps]?: ReactSelectProps[K]} = {
     className: "form-control",
 }
 
@@ -29,5 +30,5 @@ export class Select<TValue = OptionValues> extends React.Component<ReactSelectPr
         return <this.PlainSelect {...childProps} />
     }
 
-    protected handleChange: OnChangeHandler<TValue> = (option: Option<TValue>) => this.context.onChange(option.value);
+    protected handleChange: OnChangeHandler<TValue> = handleChange.bind(this);
 }
